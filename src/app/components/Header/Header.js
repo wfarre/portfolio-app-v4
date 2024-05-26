99c43b2d4b93c1dcf158f81c6a802901d4803fd9
wfarre <william.farre@gmail.com>
2024-05-26 11:58:11 +0800

    first commit

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,49 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import React from "react";
+import HeroImage from "@/assets/images/hero-image.svg";
+
+const Header = () => {
+  return (
+    <header className=" mx-40 mt-32 ">
+      <div className="flex gap-4 items-center">
+        <ul className="flex flex-col gap-3 w-fit max-w-[500px]">
+          <li>
+            <h1 className="text-6xl font-bold text-slate-900">
+              Hello, <br /> {"I'm "}
+              <span className="bg-gradient-to-tr from-teal-500 to-teal-200 bg-clip-text text-transparent">
+                William
+              </span>
+              .
+            </h1>
+          </li>
+          <li>
+            <p className="font-bold text-xl">Front-End Web Developer</p>
+          </li>
+          <li>
+            <p>
+              Thankfully, I am a web developer who enjoy building websites and
+              web applications. My main stack is Google searches! Besides
+              Google, I generally use React or Svelte.
+            </p>
+          </li>
+          <li className="mt-3">
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              className="bg-gradient-to-tr from-teal-500 to-teal-200 py-2 px-6 rounded-lg text-white font-bold uppercase"
+            >
+              Contact me
+            </motion.button>
+          </li>
+        </ul>
+        <div>
+          <Image src={HeroImage} alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,57 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const links = [
+  {
+    text: "About",
+    path: "/about",
+  },
+  {
+    text: "Projects",
+    path: "/projects",
+  },
+  {
+    text: "Contact",
+    path: "/contact",
+  },
+];
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="mx-40 py-10">
+      <ul className="flex justify-end gap-4 align-middle">
+        <li className="mr-auto">
+          <motion.div whileHover={{ scale: 1.2 }}>
+            <Link
+              className="text-lg font-bold bg-gradient-to-tr from-teal-500 to-teal-200 p-2 text-white rounded-lg"
+              href="/"
+            >
+              WF
+            </Link>
+          </motion.div>
+        </li>
+
+        {links.map((link, index) => {
+          return (
+            <li
+              className="relative after:absolute after:w-full after:bottom-0 after:left-0 after:h-[2px] after:bg-teal-500 after:scale-0 hover:after:scale-100 after:transition-all after:duration-500"
+              key={link.text + index}
+            >
+              <Link
+                className="hover:text-teal-500 transition-colors duration-300"
+                href={link.path}
+              >
+                {link.text}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
