34b35db47003aa09dbf703edc65918caf0632bac
wfarre <william.farre@gmail.com>
2024-06-02 14:08:56 +0800

    jj

--- src/app/page.js
+++ src/app/page.js
@@ -306,13 +306,13 @@ function useWindowSize() {
     // Add event listener
     if (window !== undefined) {
       window?.addEventListener("resize", handleResize);
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+
+      // Remove event listener on cleanup
+      return () => window?.removeEventListener("resize", handleResize);
     }
-
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-
-    // Remove event listener on cleanup
-    return () => window?.removeEventListener("resize", handleResize);
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
 }
